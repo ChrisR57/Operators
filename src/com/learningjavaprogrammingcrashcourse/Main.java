9158d7d7ef3c71f8c628a5ac0cbf124ec5b45e55
ChrisR57 <cr_cbr@outlook.com>
2019-11-01 09:46:59 +0000

    116. Operator Precedence

--- src/com/learningjavaprogrammingcrashcourse/Main.java
+++ src/com/learningjavaprogrammingcrashcourse/Main.java
@@ -4,42 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        int answer = (7 + 3) * 4;
-//        System.out.println(answer);
-//
-//        double radius = 12.0;
-//        double area = Math.PI * radius * radius;
+        double a = 12 * 3 / 4;
+        double b = (12 * 3) / 4;
+        double c = 12 * (3 / 4);
+        System.out.printf("%s %s %s %n", a, b, c);
+        System.out.println();
 
-        int x = 3;
-//        int y = x++;
-        int y = x;
-        x++;
-        System.out.printf("Using x++, x is %s and y is %s.%n", x, y);
-        // Using x++, x is 4 and y is 3
+        double d = 12.0 / 3.0 / 4.0;
+        double e = (12.0 / 3.0) / 4.0;
+        double f = 12.0 / (3.0 / 4.0);
+        System.out.printf("%s %s %s %n", d, e, f);
+        System.out.println();
 
-        x = 3;
-//        y = ++x;
-        x++;
-        y = x;
-        System.out.printf("Using ++x, x is %s and y is %s.%n", x, y);
-        // Using x++, x is 4 and y is 4
+        int x = 12 + 3 * 4;
+        int y = (12 + 3) * 4;
+        int z = 12 + (3 * 4);
+        System.out.printf("%s %s %s %n", x, y, z);
 
-        System.out.println("Don't do this!");
-        x = 3;
-//        y = ++x - x++;
-        x++;
-        int z = x;
-        y = z - x;      // or just y = 0;
-        x++;
-        System.out.printf("++x - x++ gives: x is %s and y is %s.%n", x, y);
-        // ++x -  x++ gives x is 5 and y is 0
 
-        x = 3;
-//        y = x++ - ++x;
-        z = x;
-        x += 2;
-        y = z - x;    // or just y = -2;
-        System.out.printf("x++ - ++x gives: x is %s and y is %s.%n", x, y);
-        // x++ -  ++x gives x is 5 and y is -2
     }
 }
