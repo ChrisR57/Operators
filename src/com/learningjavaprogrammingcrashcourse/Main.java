c789711bd0d9d59f845d25ccb85a61c4a11d484d
ChrisR57 <cr_cbr@outlook.com>
2019-11-01 10:13:33 +0000

    117. Using the remainder Operator

--- src/com/learningjavaprogrammingcrashcourse/Main.java
+++ src/com/learningjavaprogrammingcrashcourse/Main.java
@@ -4,32 +4,49 @@ public class Main {
 
     public static void main(String[] args) {
 
-     // Integer division only uses integer part of calculations so 3/4 = 0
-        // where as Floating point would be 3/4  = 0.75
-        double a = 12 * 3 / 4;
-        double b = (12 * 3) / 4;
-        double c = 12 * (3 / 4);
-        System.out.printf("%s %s %s %n", a, b, c);
-        System.out.println();
+        int x = 12;
+        int y = 5;
 
-     //Floating point division
-         a = 12 * 3 / 4.0;
-         b = (12 * 3) / 4.0;
-         c = 12 * (3 / 4.0);
-        System.out.printf("%s %s %s %n", a, b, c);
-        System.out.println();
-
-        double d = 12.0 / 3.0 / 4.0;
-        double e = (12.0 / 3.0) / 4.0;
-        double f = 12.0 / (3.0 / 4.0);
-        System.out.printf("%s %s %s %n", d, e, f);
-        System.out.println();
-
-        int x = 12 + 3 * 4;
-        int y = (12 + 3) * 4;
-        int z = 12 + (3 * 4);
-        System.out.printf("%s %s %s %n", x, y, z);
-
-
+        if (x % y == 0) {
+            System.out.printf("%s divides exactly into %s%n", y, x);
+        } else {
+            System.out.printf("%s does not divide exactly into %s%n", y, x);
+        }
+        
+        for (int month = 1; month <=12; month++) {
+            System.out.printf("The next month after %s is %s%n", month, month % 12 + 1);
+        }
     }
+
+
+
+
+//     // Integer division only uses integer part of calculations so 3/4 = 0
+//        // where as Floating point would be 3/4  = 0.75
+//        double a = 12 * 3 / 4;
+//        double b = (12 * 3) / 4;
+//        double c = 12 * (3 / 4);
+//        System.out.printf("%s %s %s %n", a, b, c);
+//        System.out.println();
+//
+//     //Floating point division
+//         a = 12 * 3 / 4.0;
+//         b = (12 * 3) / 4.0;
+//         c = 12 * (3 / 4.0);
+//        System.out.printf("%s %s %s %n", a, b, c);
+//        System.out.println();
+//
+//        double d = 12.0 / 3.0 / 4.0;
+//        double e = (12.0 / 3.0) / 4.0;
+//        double f = 12.0 / (3.0 / 4.0);
+//        System.out.printf("%s %s %s %n", d, e, f);
+//        System.out.println();
+//
+//        int x = 12 + 3 * 4;
+//        int y = (12 + 3) * 4;
+//        int z = 12 + (3 * 4);
+//        System.out.printf("%s %s %s %n", x, y, z);
+//
+//
+//    }
 }
