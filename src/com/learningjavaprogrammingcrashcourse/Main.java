77dc75b4873b196b14dd5f51a4f21a5b68d38800
ChrisR57 <cr_cbr@outlook.com>
2019-11-01 10:01:13 +0000

    116. Operator Precedence Integer / Floating point examples

--- src/com/learningjavaprogrammingcrashcourse/Main.java
+++ src/com/learningjavaprogrammingcrashcourse/Main.java
@@ -4,9 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
+     // Integer division only uses integer part of calculations so 3/4 = 0
+        // where as Floating point would be 3/4  = 0.75
         double a = 12 * 3 / 4;
         double b = (12 * 3) / 4;
         double c = 12 * (3 / 4);
+        System.out.printf("%s %s %s %n", a, b, c);
+        System.out.println();
+
+     //Floating point division
+         a = 12 * 3 / 4.0;
+         b = (12 * 3) / 4.0;
+         c = 12 * (3 / 4.0);
         System.out.printf("%s %s %s %n", a, b, c);
         System.out.println();
 
